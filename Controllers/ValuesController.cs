ba82af7b3515f5db491ee1b981d73c53f1036dfa
Shrivalli Maheshwaran <shrivalli04@gmail.com>
2020-09-24 22:16:39 +0530

    valuescontroller added

---
+++ Controllers/ValuesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DevopsParishaPrj.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public List<string> ls = new List<string>();
+        
+        [Route("getnames")]
+        [HttpGet]
+        public List<string> getnames()
+        {
+            ls.Add("Ram");
+            ls.Add("Radha");
+            return ls;
+        }
+    }
+}
